4e998345aef9cdaa7642b72d186b74db2b7b18d3
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-07-29 18:28:28 +0200

    my brain is decentralized

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const crypto = require("crypto");
 const express = require("express");
-const db = require('./mydb')
+const db = require("./mydb");
 const { PrismaClient } = require("@prisma/client");
-
 
 const IP = "172.30.53.11";
 const PORT = 3333;
@@ -13,22 +12,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false })); // to support URL-encoded bodies
 app.use(express.json()); // to support JSON-encoded bodies
 
-
 // A middle for checking if an api key is provided by the user
 // If an api key is provided in the authorization header field then
 // the api key is attached to the req object
 const getApiKey = async (req, res, next) => {
-  const apiKey = req.headers.authorization
+  const apiKey = req.headers.authorization;
   if (!apiKey) {
     res.status(403).json({
-      status: 'fail',
-      data: { apiKey: 'No api key in Authorization header' },
-    })
+      status: "fail",
+      data: { apiKey: "No api key in Authorization header" },
+    });
   } else {
-    req.apiKey = apiKey.replace('Bearer ', '').trim()
-    next()
+    req.apiKey = apiKey.replace("Bearer ", "").trim();
+    next();
   }
-}
+};
 
 // A middleware for checking if an api key is valid
 // and is still active.
@@ -36,105 +34,121 @@ const getApiKey = async (req, res, next) => {
 
 const validateApiKey = async (req, res, next) => {
   try {
-    const result = await db.getUserByApiKey(req.apiKey)
+    const result = await db.getUserByApiKey(req.apiKey);
     // Check if user is active
     // check if null result then not found
     if (!result || !result.active) {
-      res.status(403).json({ status: 'fail', data: { key: 'Invalid api key' } })
+      res
+        .status(403)
+        .json({ status: "fail", data: { key: "Invalid api key" } });
     } else {
-      req.userId = result.id
-      next()
+      req.userId = result.id;
+      next();
     }
   } catch (e) {
-    console.log(e)
-    res.status(500).json({ code: 'error', message: 'Internal server error' })
+    console.log(e);
+    res.status(500).json({ code: "error", message: "Internal server error" });
   }
-}
+};
 
-app.use(express.urlencoded({ extended: false })) // to support URL-encoded bodies
-app.use(express.json()) // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: false })); // to support URL-encoded bodies
+app.use(express.json()); // to support JSON-encoded bodies
 
-app.post('/register', async (req, res) => {
-  const username = req.body.username
-  const email = req.body.email
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
   try {
-    const result = await db.register(username, email)
+    const result = await db.register(username, email);
     res.json({
-      status: 'success',
+      status: "success",
       data: { id: result.id, key: result.apiKey.key },
-    })
+    });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.use(getApiKey)
-app.use(validateApiKey)
+app.use(getApiKey);
+app.use(validateApiKey);
 
-app.get('/user_by_id/:userId', async (req, res) => {
-  let userId = req.params.userId
+app.get("/user_by_id/:userId", async (req, res) => {
+  let userId = req.params.userId;
   if (isNaN(userId)) {
-    res.json({ status: 'fail', data: { userId: `${userId} is not a number` } })
-    return
+    res.json({ status: "fail", data: { userId: `${userId} is not a number` } });
+    return;
   }
-  userId = Number(userId)
+  userId = Number(userId);
   try {
-    const result = await db.getUserById(userId)
-    res.json({ status: 'success', data: { user: result } })
+    const result = await db.getUserById(userId);
+    res.json({ status: "success", data: { user: result } });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.get('/myinfo', async (req, res) => {
-  const userId = req.userId
+app.get("/myinfo", async (req, res) => {
+  const userId = req.userId;
   try {
-    const result = await db.getUserById(userId)
-    res.json({ status: 'success', data: { user: result } })
+    const result = await db.getUserById(userId);
+    res.json({ status: "success", data: { user: result } });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.get('/user_by_username/:username', async (req, res) => {
+app.get("/user_by_username/:username", async (req, res) => {
   // A implementer
-})
+});
 
-app.post('/send_message/:username', async (req, res) => {
+app.post("/send_message/:username", async (req, res) => {
   try {
     const srcId = await db.getIdByApiKey(req.apiKey);
     const dstId = await db.getIdByUsername(req.params.username);
     const content = await req.body.content;
     const message = await db.sendMessage(srcId, dstId, content);
-    res.json({ status: 'success', data: { message: message, src: srcId, dst: dstId, content: content } })
+    res.json({
+      status: "success",
+      data: { message: message, src: srcId, dst: dstId, content: content },
+    });
   } catch (e) {
-    if (e.status === 'fail') {
-      res.status(400).json({ status: e.status, data: e.dataError })
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
     } else {
       // e.status === 50X
-      res.status(500).json({ status: e.status, message: e.message })
+      res.status(500).json({ status: e.status, message: e.message });
     }
   }
-})
+});
 
-app.get('/read_message/', async (req, res) => {
-  // A implementer
-})
+app.get("/read_message/", async (req, res) => {
+  try {
+    const messages = await db.readMessages(req.apiKey);
+    console.log(messages);
+    res.json({ status: "success", data: { messages: messages } });
+  } catch (e) {
+    if (e.status === "fail") {
+      res.status(400).json({ status: e.status, data: e.dataError });
+    } else {
+      // e.status === 50X
+      res.status(500).json({ status: e.status, message: e.message });
+    }
+  }
+});
 
 app.listen(PORT, IP, () => {
   console.log(`listening on ${IP}:${PORT}`);
--- mydb.js
+++ mydb.js
@@ -1,27 +1,27 @@
-const crypto = require('crypto')
-const { PrismaClient, Prisma } = require('@prisma/client')
-const prisma = new PrismaClient()
+const crypto = require("crypto");
+const { PrismaClient, Prisma } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const customizeError = (e) => {
   // A query error
   if (e instanceof Prisma.PrismaClientKnownRequestError) {
-    e.status = 'fail'
-    e.dataError = {}
+    e.status = "fail";
+    e.dataError = {};
     switch (e.code) {
-      case 'P2002':
-        e.dataError[e.meta.target[0]] = `${e.meta.target[0]} already exists`
-        break
+      case "P2002":
+        e.dataError[e.meta.target[0]] = `${e.meta.target[0]} already exists`;
+        break;
       default:
-        e.dataError[e.meta.target[0]] = e.message
+        e.dataError[e.meta.target[0]] = e.message;
     }
   } else {
-    e.status = 'error'
+    e.status = "error";
   }
-  throw e
-}
+  throw e;
+};
 
 exports.register = async (username, email) => {
-  const apiKey = crypto.randomUUID()
+  const apiKey = crypto.randomUUID();
   try {
     const result = await prisma.user.create({
       data: {
@@ -33,7 +33,7 @@ exports.register = async (username, email) => {
           },
         },
       },
-    })
+    });
 
     return await prisma.user.findUnique({
       where: {
@@ -47,12 +47,12 @@ exports.register = async (username, email) => {
           },
         },
       },
-    })
+    });
   } catch (e) {
-    customizeError(e)
-    throw e
+    customizeError(e);
+    throw e;
   }
-}
+};
 
 exports.getUserByApiKey = async (apiKey) => {
   try {
@@ -65,7 +65,7 @@ exports.getUserByApiKey = async (apiKey) => {
           },
         },
       },
-    })
+    });
     /* 2eme alternative: result est différent
     const result =  await prisma.apiKey.findUnique({
       where: {
@@ -75,12 +75,12 @@ exports.getUserByApiKey = async (apiKey) => {
         user: true,
       },
     })*/
-    return result
+    return result;
   } catch (e) {
-    customizeError(e)
-    throw e
+    customizeError(e);
+    throw e;
   }
-}
+};
 
 exports.getUserById = async (userId) => {
   try {
@@ -88,13 +88,13 @@ exports.getUserById = async (userId) => {
       where: {
         id: userId,
       },
-    })
-    return result
+    });
+    return result;
   } catch (e) {
-    customizeError(e)
-    throw e
+    customizeError(e);
+    throw e;
   }
-}
+};
 
 exports.getUserByUsername = async (username) => {
   try {
@@ -102,13 +102,13 @@ exports.getUserByUsername = async (username) => {
       where: {
         username: username,
       },
-    })
-    return result
+    });
+    return result;
   } catch (e) {
-    customizeError(e)
-    throw e
+    customizeError(e);
+    throw e;
   }
-}
+};
 
 exports.sendMessage = async (srcId, dstId, content) => {
   try {
@@ -118,31 +118,51 @@ exports.sendMessage = async (srcId, dstId, content) => {
         dstId: dstId,
         content: content,
       },
-    })
+    });
     return message;
   } catch (e) {
-    console.log(e)
-    customizeError(e)
-    throw e
+    console.log(e);
+    customizeError(e);
+    throw e;
   }
-}
+};
+
+exports.readMessages = async (apiKey) => {
+  try {
+    const username = await this.getUserByApiKey(apiKey);
+    console.log(username)
+    const id = username.id;
+    console.log(id)
+    const messages = await prisma.message.findMany({
+      where: {
+        srcId: id,
+      },
+    });
+    console.log(messages)
+    return messages;
+  } catch (e) {
+    console.log(e);
+    customizeError(e);
+    throw e;
+  }
+};
 
 exports.getIdByApiKey = async (apiKey) => {
   try {
-    const  username  = await this.getUserByApiKey(apiKey);
+    const username = await this.getUserByApiKey(apiKey);
     return username.id;
   } catch (e) {
-    customizeError(e)
-    throw e
+    customizeError(e);
+    throw e;
   }
-}
+};
 
 exports.getIdByUsername = async (username) => {
   try {
-    const  user = await this.getUserByUsername(username);
+    const user = await this.getUserByUsername(username);
     return user.id;
   } catch (e) {
-    customizeError(e)
-    throw e
+    customizeError(e);
+    throw e;
   }
-}
+};
